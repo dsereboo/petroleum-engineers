ae343b8c01303839c119eb62096b9e099065faa1
dsereboo <dsereboo@gmail.com>
2022-02-05 22:56:47 +0000

    Base projects page

---
+++ pages/projects.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout"
+
+const Projects=()=>{
+    return(
+        <Layout>
+            <section className="hero bg-img">
+            </section>  
+        </Layout>
+    )
+}
+
+export default Projects
